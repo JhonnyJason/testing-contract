50ce6920fa1d256133760eb440d5d1f129333eed
Lenard Frommelt <lenny09918050@gmail.com>
2020-12-27 08:08:40 +0100

    reduced to some minimum

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,8 +1,5 @@
-var MultiSig2of3 = artifacts.require("MultiSig2of3");
-var PrivateCounter = artifacts.require("PrivateCounter")
+var Counter = artifacts.require("Counter")
 
 module.exports = function(deployer, network, accounts) {
-    var allWallets = accounts.slice(0,3)
-    deployer.deploy(MultiSig2of3, allWallets)
-    deployer.deploy(PrivateCounter)
+    deployer.deploy(Counter)
 };
